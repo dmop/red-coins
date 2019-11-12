a8e6857d8df402f881e25ba2b8cb3e3c6835c971
dmop <danilomopedrosa@gmail.com>
2019-11-11 23:11:51 -0300

    add transaction store

---
+++ app/models/transaction.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"red-coins/config"
+	"time"
+)
+
+type Transaction struct {
+	ID        int    	`gorm:"primary_key;auto_increment" json:"id"`
+	Cotation  float64   `validate:"required" gorm:"not null" json:"cotation"`
+	Amount    float64   `validate:"required" gorm:"not null;" json:"amount"`
+	Type      string    `validate:"required" gorm:"not null;" json:"type"`
+	UserId    int    	`validate:"required" gorm:"not null" json:"user_id"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+
+func TransactionStore(transaction *Transaction) bool {
+	res := config.DB.Debug().Create(&transaction)
+
+	if res.Error == nil {
+		return true
+	}
+
+	return false
+}
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "github.com/dmop/red-coins/api"
+import (
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/labstack/echo/v4"
+	"gopkg.in/tylerb/graceful.v1"
+	"red-coins/app/console"
+	"red-coins/app/router"
+	"red-coins/config"
+	"red-coins/migrations"
+	"time"
+)
+
+// @title redCoins
+// @version 1.0
+// @description This is a sample server
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 
 func main() {
-	api.Run()
+	app := echo.New()
+
+	db := config.Database()
+	
+	migrations.Migrate(db)
+
+	defer db.Close()
+
+	config.Redis()
+	console.Schedule()
+	router.Init(app)
+
+	app.Server.Addr = ":3333"
+	graceful.ListenAndServe(app.Server, 5*time.Second)
 }
